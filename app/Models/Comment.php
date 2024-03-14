1afaf7f8d5c31c3f21342d288a3cd84a286e5e21
saeed <saeedmouzarmi@gmail.com>
2024-03-14 19:20:36 +0330

    updated models

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,9 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
     use HasFactory;
-    protected $guarded
+
+    protected $guarded = ["id"];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+
 }
--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Conversation extends Model
 {
     use HasFactory;
+
+    protected $guarded = ["id"];
+
+
+    public function starter(): BelongsTo
+    {
+        return $this->belongsTo(User::class, "user1_id");
+    }
+
+    public function receiver(): BelongsTo
+    {
+        return $this->belongsTo(User::class, "user2_id");
+    }
+
+    public function messages(): HasMany
+    {
+        return $this->hasMany(Message::class);
+    }
 }
