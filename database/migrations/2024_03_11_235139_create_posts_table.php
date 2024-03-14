04707113f403311c741e884afc614a6ca6560df7
saeed <saeedmouzarmi@gmail.com>
2024-03-14 23:04:13 +0330

    Added default enum value for migrations.

--- database/migrations/2024_03_11_235139_create_posts_table.php
+++ database/migrations/2024_03_11_235139_create_posts_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\PostVisibilityEnum;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -15,7 +16,7 @@ return new class extends Migration {
             $table->id();
             $table->foreignIdFor(User::class)->constrained();
             $table->longText("content");
-            $table->string("visibility")->default("public");
+            $table->string("visibility")->default(PostVisibilityEnum::getDefaultItem());
             $table->softDeletes();
             $table->timestamps();
         });
--- database/migrations/2024_03_11_235206_create_media_table.php
+++ database/migrations/2024_03_11_235206_create_media_table.php
@@ -1,12 +1,12 @@
 <?php
 
+use App\Enums\MediaTypeEnum;
 use App\Models\Post;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->id();
             $table->foreignIdFor(Post::class)->constrained();
             $table->longText("file_path");
-            $table->string("media_type")->default("image");
+            $table->string("media_type")->default(MediaTypeEnum::getDefaultItem());
             $table->softDeletes();
             $table->timestamps();
         });
